d453456747c4d8cee878550b8d5b18fdec552511
Daniel <danielclark.projects@gmail.com>
2023-03-14 12:47:41 +0100

    Game Card Image Icon

--- src/components/game-list/GameCard.jsx
+++ src/components/game-list/GameCard.jsx
@@ -1,13 +1,23 @@
 import styles from './GameCard.module.css';
 
+const GAME_TYPE_ICONS = {
+	'finding-pairs': { text: 'Finding Pairs ', image: '/images/game-cards.png' },
+	hangman: { text: 'Hangman', image: '/images/game-letters.png' },
+	quiz: { text: 'Quiz', image: '/images/game-quiz.png' },
+	gambling: { text: 'Gambling', image: '/images/game-gambling.png' }
+};
+
 const GameCard = ({ game }) => {
+	const { text, image } = GAME_TYPE_ICONS[game.type];
+
 	return (
 		<div className={styles.card}>
-			<img className={styles.image} src={'/images/game.svg'}></img>
+			<img className={styles.image} src={image}></img>
 
 			<div className={styles.info}>
 				<span className={styles.title}>{game.title}</span>
-				<span className={styles.text}>{`Created by: ${game.author}`}</span>
+				<span className={styles.type}>{text}</span>
+				<span className={styles.text}>{`Created by: ${game.userId}`}</span>
 				<span className={styles.text}>{`${game.pairs.length} pairs`}</span>
 			</div>
 		</div>
