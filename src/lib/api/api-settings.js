4e9fdecd2e9f29285bda1307fbc5a8d45ca4e166
Daniel <danielclark.projects@gmail.com>
2023-04-08 14:51:12 +0200

    Test url production

--- src/lib/api/api-settings.js
+++ src/lib/api/api-settings.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'learning-with-games-api.netlify.app'; // 'http://localhost:8080';
+export const API_URL_PRO = 'learning-with-games-api.netlify.app';
 export const API_VERSION = 'v1';
