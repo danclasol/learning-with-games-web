0adc7706b7e2f8097666b0474ce0f5a162a6948f
Daniel <danielclark.projects@gmail.com>
2023-04-11 11:35:17 +0200

    Set API URL using var env production/development

--- src/lib/api/api-settings.js
+++ src/lib/api/api-settings.js
@@ -1,3 +1,4 @@
-export const API_URL =
-	'https://learning-with-games-server-production.up.railway.app'; // 'http://localhost:8080';
+export const API_URL = import.meta.env.PROD
+	? 'https://learning-with-games-server-production.up.railway.app'
+	: 'http://localhost:8080';
 export const API_VERSION = 'v1';
