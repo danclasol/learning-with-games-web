14a255b088e49ea29661b59461dd2dedb81c4096
Daniel <danielclark.projects@gmail.com>
2023-04-19 18:03:22 +0200

    Finding Pairs Add Mode field

--- src/components/game-finding-pairs/edit/GameEdit.jsx
+++ src/components/game-finding-pairs/edit/GameEdit.jsx
@@ -5,9 +5,11 @@ import InputText from '../../forms/InputText';
 import GameEditActions from '../../games/GameEditActions';
 
 import { useContext } from 'react';
+import { FINDING_PAIRS_MODES } from '../../../constants/findingPairsModes';
 import { AuthContext } from '../../../lib/context/AuthContext';
+import InputSelect from '../../forms/InputSelect';
 import styles from './GameEdit.module.css';
-import PairCardEdit from './PairCardEdit';
+import PairImageCardEdit from './PairImageCardEdit';
 
 const GameEdit = ({ game }) => {
 	const { accessToken } = useContext(AuthContext);
@@ -18,7 +20,9 @@ const GameEdit = ({ game }) => {
 		watch,
 		reset,
 		formState: { errors, isDirty, isSubmitting }
-	} = useForm({ defaultValues: { title: game?.title, pairs: game?.pairs } });
+	} = useForm({
+		defaultValues: { title: game?.title, mode: game?.mode, pairs: game?.pairs }
+	});
 
 	const { fields, prepend, remove } = useFieldArray({
 		name: 'pairs',
@@ -57,21 +61,40 @@ const GameEdit = ({ game }) => {
 					})}
 				>
 					<div className={styles.game__info}>
-						<div className={styles.form__field}>
-							<InputText
-								name='title'
-								label='Title'
-								placeholder='Title'
-								register={register}
-								validate={{
-									required: 'Field required',
-									minLength: {
-										value: 4,
-										message: 'At least 4 characters'
-									}
-								}}
-								error={errors.title?.message}
-							/>
+						<div className={styles.form__fields}>
+							<div className={styles.form__field}>
+								<InputText
+									name='title'
+									label='Title'
+									placeholder='Title'
+									register={register}
+									validate={{
+										required: 'Field required',
+										minLength: {
+											value: 4,
+											message: 'At least 4 characters'
+										}
+									}}
+									error={errors.title?.message}
+								/>
+							</div>
+							<div className={styles.form__field}>
+								<InputSelect
+									name='mode'
+									label='Mode'
+									register={register}
+									error={errors.mode?.message}
+								>
+									<option value=''>Select mode...</option>
+									{Object.values(FINDING_PAIRS_MODES).map(item => {
+										return (
+											<option key={item.type} value={item.type}>
+												{item.name}
+											</option>
+										);
+									})}
+								</InputSelect>
+							</div>
 						</div>
 					</div>
 
@@ -85,7 +108,11 @@ const GameEdit = ({ game }) => {
 						</div>
 						<div className={styles.pairs__list}>
 							{fields.map((field, index) => (
-								<PairCardEdit key={field.id} index={index} control={control} />
+								<PairImageCardEdit
+									key={field.id}
+									index={index}
+									control={control}
+								/>
 							))}
 						</div>
 					</div>
---
+++ src/constants/findingPairsModes.js
@@ -0,0 +1,10 @@
+export const FINDING_PAIRS_MODES = {
+	DUPLICATE_IMAGES: {
+		type: 'duplicate_images',
+		name: 'Duplicate images'
+	},
+	NO_DUPLICATE_IMAGES: {
+		type: 'no_duplicate_image',
+		name: 'No duplicate images'
+	}
+};
