143553418f69bd0c4095d9674d38b9c76c1bad1b
Daniel <danielclark.projects@gmail.com>
2023-05-02 11:42:00 +0200

    Change host server (backend)

--- src/lib/api/api-settings.js
+++ src/lib/api/api-settings.js
@@ -1,4 +1,5 @@
 export const API_URL = import.meta.env.PROD
-	? 'https://learning-with-games-server-production.up.railway.app'
+	? // ? 'https://learning-with-games-server-production.up.railway.app'
+	  'https://learning-with-games-server.onrender.com'
 	: 'http://localhost:8080';
 export const API_VERSION = 'v1';
